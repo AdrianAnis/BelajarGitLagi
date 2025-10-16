c94cc2d26637c6dfe181b48397d9f17624c7636d
AdrianAnis <adriananis06@gmail.com>
2025-10-16 12:46:40 +0700

    Menambahkan fitur baru di Branch2

--- tes.cpp
+++ tes.cpp
@@ -3,16 +3,29 @@ using namespace std;
 
 int main() {
     int a, b, hasil;
+    int pilihan;
 
     cout << "Masukkan bilangan pertama: ";
     cin >> a;
-
     cout << "Masukkan bilangan kedua: ";
     cin >> b;
 
-    hasil = a + b;
+    cout <<"Masukkann Pilihan Operasi (+, -, *, /): " << endl;
+    cout <<"1. Penjumlahan (+)" << endl;
+    cout <<"2. Perkalian (*)" << endl;
+    cout <<"Masukkan Pilihan Operasi: ";
+    cin >> pilihan;
 
-    cout << "Hasil penjumlahan: " << hasil << endl;
+    if (pilihan == 1 ) {
+        hasil = a + b;
+    } else if (pilihan == 2) {
+        hasil = a * b;
+    } else {
+        cout << "Operasi tidak valid!" << endl;
+        return 1;
+    }
+
+    cout << "Hasil : " << hasil << endl;
 
     return 0;
 }
